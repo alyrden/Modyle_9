5d7c722579af0273edad36917bd7855e8f3701e8
alyrden <f50knox@gmail.com>
2024-12-23 22:11:10 +0300

    9

--- Modyle_9.1.py
+++ Modyle_9.1.py
@@ -1,1 +1,12 @@
-1
+def apply_all_func(self, int_list, *functions, sum, len, sorted):
+    self.int_list = list(int, float(int_list))
+    self.__functions = map(functions)
+    return dict{name:int_list, result}
+
+def apply_all_func(self, result, functions=None):
+    self.result = functions
+    for i in result:
+        return result
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
